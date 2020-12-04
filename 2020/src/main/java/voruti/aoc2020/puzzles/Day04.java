b7a3b02f017ee23d64f2d37bbc499f4c302ac039
voruti <45130096+voruti@users.noreply.github.com>
2020-12-04 13:20:42 +0100

    firstPuzzle

--- 2020/src/main/java/voruti/aoc2020/puzzles/Day04.java
+++ 2020/src/main/java/voruti/aoc2020/puzzles/Day04.java
@@ -2,8 +2,8 @@ package voruti.aoc2020.puzzles;
 
 import java.util.Arrays;
 import java.util.List;
-
-import voruti.aoc2020.utility.TwoInts;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Puzzles of day 4.
@@ -19,10 +19,44 @@ public class Day04 extends AbstractPuzzle {
      * @return the result of the calculation, etc.
      */
     public String firstPuzzle() {
-        
+        List<String> passportList = Arrays.asList(getInput().split("\n\n"));
+
+        int validCount = 0;
+
+        passportList.forEach(p -> {
+            List<String> fieldList = Arrays.asList(p.split("[\s\n]+"));
+            Map<String, String> fieldMap = fieldListToMap(fieldList);
+
+            if (fieldMap.get("byr", null) != null && 
+                fieldMap.get("iyr", null) != null && 
+                fieldMap.get("eyr", null) != null && 
+                fieldMap.get("hgt", null) != null && 
+                fieldMap.get("hcl", null) != null && 
+                fieldMap.get("ecl", null) != null && 
+                fieldMap.get("pid", null) != null) {
+                validCount++;
+            }
+        });
 
         // return:
-        return "";
+        return validCount + "";
+    }
+    
+    /**
+     * Map a {@link List} of fields to a {@link Map} of fields.
+     *
+     * @param fieldList the {@link List} containing the fields
+     * @return the {@link Map} with the mapped values of the {@code fieldList}
+     */
+    private Map<String, String> fieldListToMap(List<String> fieldList) {
+        Map<String, String> map = new HashMap<>();
+
+        fieldList.forEach(f -> {
+            String[] splitField = f.split(":");
+            map.put(splitField[0], splitField[1]);
+        });
+
+        return map;
     }
 
     /**
