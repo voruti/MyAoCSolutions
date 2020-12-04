7b99fce8852af0a98fdb221b206cf740897ac18b
voruti <45130096+voruti@users.noreply.github.com>
2020-12-04 13:54:26 +0100

    secondPuzzle

--- 2020/src/main/java/voruti/aoc2020/puzzles/Day04.java
+++ 2020/src/main/java/voruti/aoc2020/puzzles/Day04.java
@@ -66,10 +66,49 @@ public class Day04 extends AbstractPuzzle {
      * @return the result of the calculation, etc.
      */
     public String secondPuzzle() {
-        
+        List<String> passportList = Arrays.asList(getInput().split("\n\n"));
+
+        int validCount = 0;
+
+        for (String p : passportList) {
+            List<String> fieldList = Arrays.asList(p.split("[\\s\\n]+"));
+            Map<String, String> fieldMap = fieldListToMap(fieldList);
+
+            if (isValidPassport(fieldMap)) {
+                validCount++;
+            }
+        }
 
         // return:
-        return "";
+        return validCount + "";
+    }
+    
+    /**
+     * Checks, if a given passport (as {@link Map}) is valid.
+     *
+     * @param fieldMap the passport to validate
+     * @return true, if the passport is valid, false otherwise
+     */
+    private boolean isValidPassport(Map<String, String> fieldMap) {
+        return fieldMap.get("byr") != null
+                && fieldMap.get("iyr") != null
+                && fieldMap.get("eyr") != null
+                && fieldMap.get("hgt") != null
+                && fieldMap.get("hcl") != null
+                && fieldMap.get("ecl") != null
+                && fieldMap.get("pid") != null
+
+                &&
+
+                fieldMap.get("byr").matches("\\d{4}") && Integer.parseInt(fieldMap.get("byr")) >= 1920 && Integer.parseInt(fieldMap.get("byr")) <= 2002
+                && fieldMap.get("iyr").matches("\\d{4}") && Integer.parseInt(fieldMap.get("iyr")) >= 2010 && Integer.parseInt(fieldMap.get("iyr")) <= 2020
+                && fieldMap.get("eyr").matches("\\d{4}") && Integer.parseInt(fieldMap.get("eyr")) >= 2020 && Integer.parseInt(fieldMap.get("eyr")) <= 2030
+                && (fieldMap.get("hgt").matches("\\d+cm") && Integer.parseInt(fieldMap.get("hgt").substring(0, fieldMap.get("hgt").length() - 2)) >= 150 && Integer.parseInt(fieldMap.get("hgt").substring(0, fieldMap.get("hgt").length() - 2)) <= 193
+                   || fieldMap.get("hgt").matches("\\d+in") && Integer.parseInt(fieldMap.get("hgt").substring(0, fieldMap.get("hgt").length() - 2)) >= 59 && Integer.parseInt(fieldMap.get("hgt").substring(0, fieldMap.get("hgt").length() - 2)) <= 76)
+                && fieldMap.get("hcl").matches("#[0-9a-f]{6}")
+                && Arrays.asList(new String[]{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}).contains(fieldMap.get("ecl"))
+                && fieldMap.get("pid").matches("\\d{9}")
+            ;
     }
 
     /**
